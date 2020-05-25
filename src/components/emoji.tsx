6850c9d952141ea1101a9d6cf5b7fa51d618b23f
priestine <priestine1.dev@gmail.com>
2020-05-25 17:30:13 +0300

    :art: Remove redundant lint ignore

--- src/components/emoji.tsx
+++ src/components/emoji.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-// eslint-disable-next-line jsx-a11y/accessible-emoji
 export const UnderConstructionEmoji = () => (
   <span role="img" aria-label="Under Construction Emoji">
     🚧
